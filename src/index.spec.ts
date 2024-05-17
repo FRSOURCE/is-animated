5d83b0074ea57fb9abc7f7008bfb7f4074bb9344
Jakub Freisler <jakub@frsource.org>
2024-05-17 11:56:20 +0200

    chore: update tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,43 +1,56 @@
+// @vitest-environment happy-dom
+
 import { promises as fs } from 'node:fs';
-import { join, extname } from 'node:path';
+import { join, extname, basename } from 'node:path';
+import { File } from 'happy-dom';
 import { it, describe } from 'vitest';
 import isAnimated from './index';
 
 const types = ['gif', 'png', 'webp'];
+const environmentTypes = ['browser', 'nodejs'];
 const TEST_DIR = join(__dirname, '..', '__tests__');
 
-describe.each(types)('when type is "%s"', (type) => {
-  it('images are animated', async ({ expect }) => {
-    const images = await fs.readdir(join(TEST_DIR, 'animated'));
-    const imagesWithCorrectExtension = images.filter(
-      (name) => extname(name).slice(1) === type,
-    );
+const browserReadFile = async (path: string) => {
+  const buffer = await fs.readFile(path);
+  return new File([buffer], basename(path)).arrayBuffer();
+};
 
-    for (const imageName of imagesWithCorrectExtension) {
-      const buffer = await fs.readFile(join(TEST_DIR, 'animated', imageName));
-      expect(isAnimated(buffer), imageName).toBe(true);
-    }
-  });
-  it('static images are not animated', async ({ expect }) => {
-    const images = await fs.readdir(join(TEST_DIR, 'static'));
-    const imagesWithCorrectExtension = images.filter(
-      (name) => extname(name).slice(1) === type,
-    );
+describe.each(environmentTypes)('when under %s environment', (env) => {
+  const readFile = env === 'browser' ? browserReadFile : fs.readFile;
 
-    for (const imageName of imagesWithCorrectExtension) {
-      const buffer = await fs.readFile(join(TEST_DIR, 'static', imageName));
-      expect(isAnimated(buffer), imageName).toBe(false);
-    }
-  });
-  it('invalid images are not invalidated', async ({ expect }) => {
-    const images = await fs.readdir(join(TEST_DIR, 'invalid'));
-    const imagesWithCorrectExtension = images.filter(
-      (name) => extname(name).slice(1) === type,
-    );
+  describe.each(types)('when type is "%s"', (type) => {
+    it('images are animated', async ({ expect }) => {
+      const images = await fs.readdir(join(TEST_DIR, 'animated'));
+      const imagesWithCorrectExtension = images.filter(
+        (name) => extname(name).slice(1) === type,
+      );
 
-    for (const imageName of imagesWithCorrectExtension) {
-      const buffer = await fs.readFile(join(TEST_DIR, 'invalid', imageName));
-      expect(isAnimated(buffer), imageName).toBe(false);
-    }
+      for (const imageName of imagesWithCorrectExtension) {
+        const buffer = await readFile(join(TEST_DIR, 'animated', imageName));
+        expect(isAnimated(buffer), imageName).toBe(true);
+      }
+    });
+    it('static images are not animated', async ({ expect }) => {
+      const images = await fs.readdir(join(TEST_DIR, 'static'));
+      const imagesWithCorrectExtension = images.filter(
+        (name) => extname(name).slice(1) === type,
+      );
+
+      for (const imageName of imagesWithCorrectExtension) {
+        const buffer = await readFile(join(TEST_DIR, 'static', imageName));
+        expect(isAnimated(buffer), imageName).toBe(false);
+      }
+    });
+    it('invalid images are not invalidated', async ({ expect }) => {
+      const images = await fs.readdir(join(TEST_DIR, 'invalid'));
+      const imagesWithCorrectExtension = images.filter(
+        (name) => extname(name).slice(1) === type,
+      );
+
+      for (const imageName of imagesWithCorrectExtension) {
+        const buffer = await readFile(join(TEST_DIR, 'invalid', imageName));
+        expect(isAnimated(buffer), imageName).toBe(false);
+      }
+    });
   });
 });
